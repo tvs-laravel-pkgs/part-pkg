c5ba9e1b2bdc9b8ac7dac61a8ce5395550e499fc
Ram <ramakrishnan@uitoux.in>
2020-06-01 12:59:46 +0530

    Update controller.js

--- src/public/themes/theme2/part-pkg/part/controller.js
+++ src/public/themes/theme2/part-pkg/part/controller.js
@@ -253,11 +253,7 @@ app.component('partForm', {
                         } else {
                             if (!res.success == true) {
                                 $('.submit').button('reset');
-                                var errors = '';
-                                for (var i in res.errors) {
-                                    errors += '<li>' + res.errors[i] + '</li>';
-                                }
-                                custom_noty('error', errors);
+                                showErrorNoty(res);
                             } else {
                                 $('.submit').button('reset');
                                 $location.path('/part-pkg/part/list');
