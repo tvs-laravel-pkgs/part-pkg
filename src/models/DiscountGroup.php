ae485e57e860ec15a0e6450b9d65fde307a38bb4
Karthick-T <56381497+KarthickT-uitoux@users.noreply.github.com>
2020-09-15 15:51:03 +0530

    group and price discount model

---
+++ src/models/DiscountGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Abs\PartPkg;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DiscountGroup extends Model
+{
+    protected $table = "discount_groups";
+}
---
+++ src/models/PriceDiscount.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Abs\PartPkg;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PriceDiscount extends Model
+{
+    protected $table = "price_discounts";
+}
