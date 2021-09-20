4c0a657737eab83225d28d91414132649c531804
rajarajan-uitoux <82135061+rajarajan-uitoux@users.noreply.github.com>
2021-09-20 12:16:48 +0530

    part price details

---
+++ src/database/migrations/2021_09_20_111859_part_pricing_details.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PartPricingDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('part_pricing_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('part_id');
+            $table->unsignedDecimal('regular_price', 12, 2)->comment('MRP used in IMS')->nullable();
+            $table->unsignedDecimal('retail_price', 12, 2);
+            $table->date('effective_from')->nullable();
+            $table->date('effective_to')->nullable();
+            $table->unsignedInteger('created_by_id')->nullable();
+            $table->unsignedInteger('updated_by_id')->nullable();
+            $table->unsignedInteger('deleted_by_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('updated_by_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('part_pricing_details');
+    }
+}
